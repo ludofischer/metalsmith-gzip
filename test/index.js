761e38a1d1a2fa195b0249dd804c17ffb5e4a17f
Umut Benzer <umutbenzer@gmail.com>
2014-10-25 12:03:10 +0300

    Add support for multimatch source filtering.

--- test/index.js
+++ test/index.js
@@ -2,27 +2,31 @@ var test = require('tape');
 var compress = require('..');
 var Metalsmith = require('metalsmith');
 
-test('HTML is compressed', function(t) {
-  t.plan(3);
-  var metalsmith = Metalsmith('test/fixtures/basic');
-  metalsmith
-      .use(compress())
-      .build(function(err, files) {
-          t.error(err, 'No build errors');
-          t.ok(files['index.html.gz'], 'The compressed file is created');
-          t.ok(files['index.html'], 'The original file is conserved');
-      });
-});
-
-test('PDF is skipped', function(t) {
-  t.plan(3);
+test('By default, everything is compressed', function(t) {
+  t.plan(5);
   var metalsmith = Metalsmith('test/fixtures/pdf');
   metalsmith
       .use(compress())
       .build(function(err, files) {
           t.error(err, 'No build errors');
+          t.ok(files['style.css.gz'], 'The compressed file is created');
+          t.ok(files['style.css'], 'The original file is conserved');
+          t.ok(files['document.pdf.gz'], 'The compressed file is created');
+          t.ok(files['document.pdf'], 'The original file is conserved');
+      });
+});
+
+test('Multimatch source definition is followed', function(t) {
+  t.plan(5);
+  var metalsmith = Metalsmith('test/fixtures/pdf');
+  metalsmith
+      .use(compress({ src: ["**/*.css"] }))
+      .build(function(err, files) {
+          t.error(err, 'No build errors');
           t.notOk(files['document.pdf.gz'], 'No compressed file is created');
           t.ok(files['document.pdf'], 'The original file is conserved');
+          t.ok(files['style.css.gz'], 'The compressed file is created');
+          t.ok(files['style.css'], 'The original file is conserved');
       });
 });
 
