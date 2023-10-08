a878adcf92d0e1592b1e7ce569c4976aeeafc3ed
Ludovico Fischer <livrerie@gmail.com>
2023-10-08 18:42:05 +0200

    test: migrate to built-in test runner

--- test/formatBytes.js
+++ test/formatBytes.js
@@ -1,20 +1,19 @@
 'use strict';
 
-const test = require('tape');
+const test = require('node:test');
+const assert = require('node:assert/strict');
 const formatBytes = require('../lib/formatBytes.js');
 
 test('Debug format bad input', function(t) {
-    t.plan(2);
-    t.ok(formatBytes(undefined) === null, 'It returns null when the input is undefined');
-    t.ok(formatBytes(true) === null, 'It returns null when the input is bad');
+    assert(formatBytes(undefined) === null, 'It returns null when the input is undefined');
+    assert(formatBytes(true) === null, 'It returns null when the input is bad');
 });
 
 test('Debug format should correctly format bytes', function(t) {
-    t.plan(6);
-    t.ok(formatBytes(23) === '23.0B', 'It formats small sizes');
-    t.ok(formatBytes(324 * (1 << 10)) === '324kB', 'It formats kilobytes');
-    t.ok(formatBytes(1 * (1 << 20)) === '1.00MB', 'It formats megabtyes');
-    t.ok(formatBytes(1 * (1 << 30)) === '1.00GB', 'It formats gigabytes');
-    t.ok(formatBytes(450 * (1 << 20)) === '450MB', 'It formats hundreds of megabytes');
-    t.ok(formatBytes(1 * (1 << 30) + 450 * (1 << 20)) === '1.44GB', 'It formats gigabytes and megabytes');
+    assert(formatBytes(23) === '23.0B', 'It formats small sizes');
+    assert(formatBytes(324 * (1 << 10)) === '324kB', 'It formats kilobytes');
+    assert(formatBytes(1 * (1 << 20)) === '1.00MB', 'It formats megabtyes');
+    assert(formatBytes(1 * (1 << 30)) === '1.00GB', 'It formats gigabytes');
+    assert(formatBytes(450 * (1 << 20)) === '450MB', 'It formats hundreds of megabytes');
+    assert(formatBytes(1 * (1 << 30) + 450 * (1 << 20)) === '1.44GB', 'It formats gigabytes and megabytes');
 });
--- test/index.js
+++ test/index.js
@@ -1,62 +1,60 @@
 'use strict';
 
-var test = require('tape');
+const test = require('node:test');
+const assert = require('node:assert/strict');
 var compress = require('..');
 var Metalsmith = require('metalsmith');
 
-test('By default, only the specified files are compressed', function(t) {
-  t.plan(9);
+test('By default, only the specified files are compressed', async function(t) {
   var metalsmith = Metalsmith('test/fixtures/pdf');
-  metalsmith
+  return metalsmith
       .env('DEBUG', process.env.DEBUG)
       .use(compress())
       .build(function(err, files) {
-          t.error(err, 'No build errors');
-          t.ok(files['style.css.gz'], 'The compressed CSS is created');
-          t.ok(files['style.css'], 'The original CSS is conserved');
-          t.ok(files['index.html.gz'], 'The compressed HTML is created');
-          t.ok(files['index.html'], 'The original HTML is conserved');
-          t.ok(files['document.svg.gz'], 'The compressed SVG is created');
-          t.ok(files['document.svg'], 'The original SVG is conserved');
-          t.notOk(files['document.pdf.gz'], 'No compressed PDF is created');
-          t.ok(files['document.pdf'], 'The original PDF is conserved');
+          assert.ifError(err, 'No build errors');
+          assert(files['style.css.gz'], 'The compressed CSS is created');
+          assert(files['style.css'], 'The original CSS is conserved');
+          assert(files['index.html.gz'], 'The compressed HTML is created');
+          assert(files['index.html'], 'The original HTML is conserved');
+          assert(files['document.svg.gz'], 'The compressed SVG is created');
+          assert(files['document.svg'], 'The original SVG is conserved');
+          assert.strictEqual(files['document.pdf.gz'], undefined, 'No compressed PDF is created');
+          assert(files['document.pdf'], 'The original PDF is conserved');
       });
 });
 
-test('Multimatch source definition is followed', function(t) {
-  t.plan(9);
+test('Multimatch source definition is followed', async function(t) {
   var metalsmith = Metalsmith('test/fixtures/pdf');
-  metalsmith
+  return metalsmith
       .env('DEBUG', process.env.DEBUG)
       .use(compress({ src: ["**/*.css", "**/*.pdf"] }))
       .build(function(err, files) {
-          t.error(err, 'No build errors');
-          t.ok(files['document.pdf.gz'], 'The compressed PDF is created');
-          t.ok(files['document.pdf'], 'The original PDF is conserved');
-          t.ok(files['style.css.gz'], 'The compressed CSS is created');
-          t.ok(files['style.css'], 'The original CSS is conserved');
-          t.notOk(files['index.html.gz'], 'No compressed HTML is created');
-          t.ok(files['index.html'], 'The original HTML is conserved');
-          t.notOk(files['document.svg.gz'], 'No compressed SVG is created');
-          t.ok(files['document.svg'], 'The original SVG is conserved');
+          assert.ifError(err, 'No build errors');
+          assert(files['document.pdf.gz'], 'The compressed PDF is created');
+          assert(files['document.pdf'], 'The original PDF is conserved');
+          assert(files['style.css.gz'], 'The compressed CSS is created');
+          assert(files['style.css'], 'The original CSS is conserved');
+          assert.strictEqual(files['index.html.gz'], undefined, 'No compressed HTML is created');
+          assert(files['index.html'], 'The original HTML is conserved');
+          assert.strictEqual(files['document.svg.gz'], undefined, 'No compressed SVG is created');
+          assert(files['document.svg'], 'The original SVG is conserved');
       });
 });
 
-test('Overwrite option has effect', function(t) {
-    t.plan(9);
+test('Overwrite option has effect', async function(t) {
     var metalsmith = Metalsmith('test/fixtures/pdf');
-    metalsmith
+    return metalsmith
       .env('DEBUG', process.env.DEBUG)
       .use(compress({overwrite: true}))
       .build(function(err, files) {
-          t.error(err, 'No build errors');
-          t.notOk(files['style.css.gz'], 'No separate compressed CSS is created');
-          t.ok(files['style.css'], 'The original CSS is conserved');
-          t.notOk(files['index.html.gz'], 'No separate compressed HTML is created');
-          t.ok(files['index.html'], 'The original HTML is conserved');
-          t.notOk(files['document.svg.gz'], 'No separate compressed SVG is created');
-          t.ok(files['document.svg'], 'The original SVG is conserved');
-          t.notOk(files['document.pdf.gz'], 'No compressed PDF is created');
-          t.ok(files['document.pdf'], 'The original PDF is conserved');
+          assert.ifError(err, 'No build errors');
+          assert.strictEqual(files['style.css.gz'], undefined, 'No separate compressed CSS is created');
+          assert(files['style.css'], 'The original CSS is conserved');
+          assert.strictEqual(files['index.html.gz'], undefined, 'No separate compressed HTML is created');
+          assert(files['index.html'], 'The original HTML is conserved');
+          assert.strictEqual(files['document.svg.gz'], undefined, 'No separate compressed SVG is created');
+          assert(files['document.svg'], 'The original SVG is conserved');
+          assert.strictEqual(files['document.pdf.gz'], undefined, 'No compressed PDF is created');
+          assert(files['document.pdf'], 'The original PDF is conserved');
       });
 });
